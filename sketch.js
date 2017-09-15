2d23a10da090f84c325770b6e55965705aa7e062
ottermegazord <idaly.ali@live.fr>
2017-09-15 12:58:09 -0400

    "adds pie chart to end of webpage"

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
+var angles = [ 30, 10, 45, 35, 60, 38, 75, 67 ];
+
 function setup() {
-  createCanvas(640, 480);
+    createCanvas(720, 400);
+    noStroke();
+    noLoop();  // Run once and stop
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+    background(100);
+    pieChart(300, angles);
+}
+
+function pieChart(diameter, data) {
+    var lastAngle = 0;
+    for (var i = 0; i < data.length; i++) {
+        var gray = map(i, 0, data.length, 0, 255);
+        fill(gray);
+        arc(width/2, height/2, diameter, diameter, lastAngle, lastAngle+radians(angles[i]));
+        lastAngle += radians(angles[i]);
+    }
 }

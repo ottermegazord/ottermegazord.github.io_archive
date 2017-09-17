943ce54e8e2d2cea98712f793bede70b8f394367
ottermegazord <idaly.ali@live.fr>
2017-09-16 23:51:20 -0400

    adds completed pizza clock

--- test/sketch.js
+++ test/sketch.js
@@ -9,6 +9,8 @@ var d = new Date();
 var big_hour_r = 120
 var small_hour_r = 80
 
+var color = map(second(), 0, 60, 0, 255)
+
 
 function setup(){
 
@@ -19,17 +21,17 @@ function setup(){
     cy = height/2;
 }
 
-function hour_hole(x, y, fill_on, fill_off, _hour, big_hour_r){
+function hour_hole(x, y, r1, g1, b1, r2, g2, b2, _hour, big_hour_r){
 
     strokeWeight(10)
-    fill(fill_on);
+    fill(r1, g2, b2);
 
     if (hour() == _hour){
-        fill(fill_on);
+        fill(r1, g1, b1);
         console.log(hour());
     }
     else{
-        fill(fill_off);
+        fill(r2, g2, b2);
     }
     ellipse(x, y, big_hour_r, big_hour_r);
 }
@@ -37,18 +39,18 @@ function hour_hole(x, y, fill_on, fill_off, _hour, big_hour_r){
 function draw(){
 
     var s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
-    var s_minute = map(hour(), 0, 60, 0, TWO_PI);
+    var s_minute = map(hour(), 0, 60, 0, TWO_PI) - HALF_PI;
 
     //crust
     fill(150,100,20)
     ellipse(cx,cy, 800,800);
 
     strokeWeight(20)
-    fill(200);
+    fill(244,75,66);
     ellipse(cx,cy, 700,700);
 
     fill(120, 23, 12)
-    minute_filler(700, s);
+    minute_filler(700, s_minute);
     strokeWeight(10)
 
     fill(255);
@@ -64,22 +66,35 @@ function draw(){
     line(cx, cy, cx + cos(s) * secondsRadius, cy + sin(s) * secondsRadius);
 
 
-    hour_hole(cx, cy - 225, 0, 255, 12, big_hour_r);
-    hour_hole(cx + 225, cy, 0, 255, 3, big_hour_r);
-    hour_hole(cx, cy + 225, 0, 255, 6, big_hour_r);
-    hour_hole(cx - 225, cy, 0, 255, 9, big_hour_r);
 
-    hour_hole(cx + 112.5, cy - 194, 0, 255, 1, small_hour_r);
-    hour_hole(cx + 194, cy - 112.5, 0, 255, 2, small_hour_r);
+    hour_hole(cx, cy - 225, 244, 66, 75, 0, 153, 151, 12, big_hour_r);
+    hour_hole(cx, cy - 225, 244, 66, 75, 0, 153, 151, 0, big_hour_r);
+    hour_hole(cx + 225, cy, 244, 66, 75, 0, 153, 151, 3, big_hour_r);
+    hour_hole(cx + 225, cy, 244, 66, 75, 0, 153, 151, 15, big_hour_r);
+    hour_hole(cx, cy + 225, 244, 66, 75, 0, 153, 151, 6, big_hour_r);
+    hour_hole(cx, cy + 225, 244, 66, 75, 0, 153, 151, 18, big_hour_r);
+    hour_hole(cx - 225, cy, 244, 66, 75, 0, 153, 151, 9, big_hour_r);
+    hour_hole(cx - 225, cy, 244, 66, 75, 0, 153, 151, 18, big_hour_r);
 
-    hour_hole(cx + 194, cy + 112.5, 0, 255, 4, small_hour_r);
-    hour_hole(cx + 112.5, cy + 194, 0, 255, 5, small_hour_r);
+    hour_hole(cx + 112.5, cy - 194, 244, 66, 75, 0, 153, 151, 1, small_hour_r);
+    hour_hole(cx + 112.5, cy - 194, 244, 66, 75, 0, 153, 151, 13, small_hour_r);
+    hour_hole(cx + 194, cy - 112.5, 244, 66, 75, 0, 153, 151, 2, small_hour_r);
+    hour_hole(cx + 194, cy - 112.5, 244, 66, 75, 0, 153, 151, 14, small_hour_r);
 
-    hour_hole(cx - 112.5, cy + 194, 0, 255, 7, small_hour_r);
-    hour_hole(cx - 194, cy + 112.5, 0, 255, 8, small_hour_r);
+    hour_hole(cx + 194, cy + 112.5, 244, 66, 75, 0, 153, 151, 4, small_hour_r);
+    hour_hole(cx + 194, cy + 112.5, 244, 66, 75, 0, 153, 151, 16, small_hour_r);
+    hour_hole(cx + 112.5, cy + 194, 244, 66, 75, 0, 153, 151, 5, small_hour_r);
+    hour_hole(cx + 112.5, cy + 194, 244, 66, 75, 0, 153, 151, 17, small_hour_r);
 
-    hour_hole(cx - 194, cy - 112.5, 0, 255, 10, small_hour_r);
-    hour_hole(cx - 112.5, cy - 194, 0, 255, 11, small_hour_r);
+    hour_hole(cx - 112.5, cy + 194, 244, 66, 75, 0, 153, 151, 7, small_hour_r);
+    hour_hole(cx - 112.5, cy + 194, 244, 66, 75, 0, 153, 151, 19, small_hour_r);
+    hour_hole(cx - 194, cy + 112.5, 244, 66, 75, 0, 153, 151, 8, small_hour_r);
+    hour_hole(cx - 194, cy + 112.5, 244, 66, 75, 0, 153, 151, 20, small_hour_r);
+
+    hour_hole(cx - 194, cy - 112.5, 244, 66, 75, 0, 153, 151, 10, small_hour_r);
+    hour_hole(cx - 194, cy - 112.5, 244, 66, 75, 0, 153, 151, 22, small_hour_r);
+    hour_hole(cx - 112.5, cy - 194, 244, 66, 75, 0, 153, 151, 11, small_hour_r);
+    hour_hole(cx - 112.5, cy - 194, 244, 66, 75, 0, 153, 151, 23, small_hour_r);
 
     //
     // var s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
@@ -89,8 +104,8 @@ function draw(){
 
 function minute_filler(diameter, data) {
 
-    fill(255,0,0)
-    arc(cx, cy, diameter, diameter, 0, data, radians(360), PIE)
+    fill(244,226,66)
+    arc(cx, cy, diameter, diameter, TWO_PI - HALF_PI, data, PIE)
 
     // var lastAngle = 0;
     // for (var i = 0; i < data.length; i++) {

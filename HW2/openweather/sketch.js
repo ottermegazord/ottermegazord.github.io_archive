a12494bd0b9f14b63a411b504fff288a5eb89401
ottermegazord <idaly.ali@live.fr>
2017-09-29 14:06:37 -0400

    Complete literal

--- HW2/openweather/sketch.js
+++ HW2/openweather/sketch.js
@@ -10,6 +10,7 @@ var temphour_1;
 var temphour_2;
 var temphour_3;
 var temphour_4;
+var windSpeed;
 
 //var searchBox = new google.maps.places.SearchBox();
 
@@ -98,8 +99,8 @@ function dayConverter(UNIX_timestamp){
 
 
 function setup() {
+    askWeather();
     createCanvas(320, 568);
-    askWeather()
     setInterval(askNews(),1000000);
     // input = createInput();
     // input.position(20, 65);
@@ -113,15 +114,23 @@ function setup() {
 
     textAlign(CENTER);
     textSize(50);
-    clear_night = loadImage('images/main-qimg-ddd632b74daa70e59cb90e13bff2af34-c.jpeg');
-    clear_sky = loadImage('images/background-21717_1280.jpg');
+    clear_night = loadImage('images/clear-night.jpeg');
+    clear_day = loadImage('images/clear-day.jpg');
+    cloudy = loadImage('images/cloudy.jpeg');
+    fog = loadImage('images/fog.jpg');
+    partly_cloudy_day = loadImage('images/partly-cloudy-day.jpg');
+    partly_cloudy_night = loadImage('images/partly-cloud-night.jpg');
+    rain = loadImage('images/rain.jpg');
+    snow = loadImage('images/snow.jpg');
+    thunderstorm = loadImage('images/thunderstorm.jpg');
+
 
     clear_day_icon = loadImage('images/clear-day.svg');
     clear_night_icon = loadImage('images/clear-night.svg');
     cloudy_icon = loadImage('images/cloudy.svg');
     fog_icon = loadImage('images/fog.svg');
-    partly_cloudy_day = loadImage('images/partly-cloudy-day.svg');
-    partly_cloudy_night = loadImage('images/partly-cloudy-night.svg');
+    partly_cloudy_day_icon = loadImage('images/partly-cloudy-day.svg');
+    partly_cloudy_night_icon = loadImage('images/partly-cloudy-night.svg');
     rain_icon = loadImage('images/rain.svg');
     snow_icon = loadImage('images/snow.svg');
     thunderstorm_icon = loadImage('images/thunderstorm.svg');
@@ -200,15 +209,15 @@ function dweatherIcon1(iconString, x, y){
             break;
 
         case 'partly-cloudy-day':
-            image(partly_cloudy_day, x, y, partly_cloudy_day.width/12, partly_cloudy_day.width/12);
+            image(partly_cloudy_day_icon, x, y, partly_cloudy_day_icon.width/12, partly_cloudy_day_icon.width/12);
             break;
 
         case 'partly-cloudy-night':
-            image(partly_cloudy_night, x, y, partly_cloudy_night.width/12, partly_cloudy_night.width/12);
+            image(partly_cloudy_night_icon, x, y, partly_cloudy_night_icon.width/12, partly_cloudy_night_icon.width/12);
             break;
 
         case 'rain':
-        image(rain_icon, x, y, rain_icon.width/12, rain_icon.width/12);
+            image(rain_icon, x, y, rain_icon.width/12, rain_icon.width/12);
             break;
 
         case 'snow':
@@ -222,6 +231,47 @@ function dweatherIcon1(iconString, x, y){
 
 }
 
+
+function mweatherIcon1(iconString, x, y, scale){
+    switch(iconString){
+        case 'clear-day':
+            image(clear_day_icon, x, y, clear_day_icon.width/scale, clear_day_icon.width/scale);
+            break;
+
+        case 'clear-night':
+            image(clear_night_icon, x, y, clear_night_icon.width/scale, clear_night_icon.width/scale);
+            break;
+
+        case 'cloudy':
+            image(cloudy_icon, x, y, cloudy_icon.width/scale, cloudy_icon.width/scale);
+            break;
+
+        case 'fog':
+            image(fog_icon, x, y, fog_icon.width/scale, fog_icon.width/scale);
+            break;
+
+        case 'partly-cloudy-day':
+            image(partly_cloudy_day_icon, x, y, partly_cloudy_day_icon.width/scale, partly_cloudy_day_icon.width/scale);
+            break;
+
+        case 'partly-cloudy-night':
+            image(partly_cloudy_night_icon, x, y, partly_cloudy_night_icon.width/scale, partly_cloudy_night_icon.width/scale);
+            break;
+
+        case 'rain':
+            image(rain_icon, x, y, rain_icon.width/scale, rain_icon.width/scale);
+            break;
+
+        case 'snow':
+            image(snow_icon, x, y, snow_icon.width/scale, snow_icon.width/scale);
+            break;
+
+        case 'thunderstorm':
+            image(thunderstorm_icon, x, y, thunderstorm_icon.width/12, thunderstorm_icon.width/12);
+            break;
+    }
+
+}
 function askWeather(){
     loadJSON(url, gotData, 'jsonp');
 }
@@ -296,9 +346,21 @@ function gotData(data) {
     day_3 = dayConverter(weather.daily.data[2].time);
     day_4 = dayConverter(weather.daily.data[3].time);
 
+    windSpeed = weather.currently.windSpeed;
+    windBearing = weather.currently.windBearing;
+    humidity = weather.currently.humidity;
+    console.log(windSpeed);
+
     //hours
 
     console.log(dicon_2);
+}
+
+function mousePressed(){
+    var d = dist(mouseX, mouseY, width/2, height / 6);
+    if (d < 100){
+        city = 'Brazil';
+    }
 }
 
 function greet(){
@@ -311,28 +373,59 @@ function draw() {
     background(0);
     switch(icon){
         case 'clear-day':
-            image(clear_sky, 0, 0, width, 5*height/8);
+            image(clear_day, 0, 0, width + random(-0.10, 0.10), 5*height/8);
             break;
-
         case 'clear-night':
             image(clear_night, 0, 0, width, 5*height/8);
+            break;
+
+        case 'cloudy':
+            image(cloudy, 0, 0, width, 5*height/8);
+            break;
+        case 'fog':
+            image(fog, 0, 0, width, 5*height/8);
+            break;
+        case 'partly-cloudy-day':
+            image(partly_cloudy_day, 0, 0, width, 5*height/8);
+            break;
+        case 'partly-cloudy-night':
+            image(partly_cloudy_night, 0, 0, width, 5*height/8);
+            break;
+        case 'rain':
+            image(rain, 0, 0, width, 5*height/8);
+            break;
+        case 'snow':
+            image(snow, 0, 0, width, 5*height/8);
+            break;
+        case 'thunderstorm':
+            image(thunderstorm, 0, 0, width, 5*height/8);
             break;
     }
 
     if (weather) {
         fill(255);
         //console.log(timer);
-        textFont('Arial', 20);
-        text(city.toUpperCase(), width/2, height / 20);
-        text(timeConverter(timer.time), width / 2, height / 4);
-        text(minutely_icon.toUpperCase(), width / 2, height / 2);
-        textFont('Arial', 8);
-        text(summary.toUpperCase(), width / 2, height / 2 + 10);
+        textFont('Arial', 30);
+        stroke(20);
+        text(city.toUpperCase(), width/2, height / 6);
+        textFont('Arial', 10);
+        text(timeConverter(timer.time), 260, 20);
+        //text(minutely_icon.toUpperCase(), width / 2, height / 2);
+        //textFont('Arial', 8);
+        //text(summary.toUpperCase(), width / 2, height / 2 + 10);
+
+        triangle(300, 100, 320, 100, 310, 80);
 
         textFont('Arial', 60);
-        text(curr_temp + '\xB0F', width / 2, height / 3 + 40);
-        textSize(5);
-        text('Min:' + min_temp + '\'\xB0F' + ' ' + 'Max: ' + max_temp + '\'\xB0F', width/2, height/ 6);
+        text(curr_temp.toFixed(0) + '\xB0F', 3 * width / 4, height / 3);
+        filter(INVERT);
+        mweatherIcon1(minutely_icon, width/4 - 60, height/3 - 85, 4);
+        filter(INVERT);
+        textSize(15);
+        text('\u21D1' + min_temp.toFixed(0) + '\xB0F ' + '\u21D1' + max_temp.toFixed(0) + '\xB0F', 3*width/4, height / 3 + 120);
+        textSize(10);
+        text('Wind Speed: ' + windSpeed + '  ' + 'Bearing: ' + windBearing, 3*width/4, height / 3 + 100);
+        text('Humidity: ' + humidity + '%', 3*width/4, height / 3 + 140);
         //console.log(news_1, news_2, news_3, news_4);
 
         textFont('Arial', 10);
@@ -377,7 +470,6 @@ function draw() {
         // weatherIcon3(dicon_3);
         // weatherIcon4(dicon_4);
         filter(INVERT);
-
 
     }
 }
